af6cc5355b1c9c9865369cb93a734b2dd357093f
John M. Daly <jmdaly@gmail.com>
2014-04-02 13:15:14 -0400

    Added a bash script to backup dotfiles and set up symlinks to the repo automatically.

---
+++ setup_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script sets up symlinks to all the dotfiles
+# in the user's home directory.
+
+# Create a backup directory:
+mkdir -p ~/.dotfiles_backup
+
+mv ~/.zshrc ~/.dotfiles_backup
+ln -s ~/git/dotfiles/zshrc ~/.zshrc
+
+mv ~/.vimrc ~/.dotfiles_backup
+ln -s ~/git/dotfiles/vimrc ~/.vimrc
+
+mv ~/.gitconfig ~/.dotfiles_backup
+ln -s ~/git/dotfiles/gitconfig ~/.gitconfig
+
+mv ~/.tmux.conf ~/.dotfiles_backup
+ln -s ~/git/dotfiles/tmux.conf ~/.tmux.conf
+
+source ~/.zshrc
