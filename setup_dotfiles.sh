e97fa2617a26172c925dffe7d724a5abd03de571
John M. Daly <jmdaly@gmail.com>
2015-03-22 14:21:24 -0400

    Modified the setup script, using improvements from Russell's script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -3,31 +3,47 @@
 # This script sets up symlinks to all the dotfiles
 # in the user's home directory.
 
+if [[ "$(which realpath)" == "" ]]; then
+	echo "Cannot find realpath.  Use apt-get to install it"
+	declare base=$(dirname $(realpath $0))
+#	exit 1;
+else
+	#declare base=/home/$(whoami)/dotfiles
+	declare base=${HOME}/dotfiles
+fi;
+
 # First ensure that the submodules in this repo
 # are available and up to date:
-cd ~/git/dotfiles
+cd ${base}
 git submodule init
 git submodule update
 cd -
 
+files=(.zshrc .vimrc .tmux.conf .gitconfig .pathrc)
+
+declare backup_dir=~/.dotfiles_backup
+
 # Create a backup directory:
 mkdir -p ~/.dotfiles_backup
 
-mv ~/.zshrc ~/.dotfiles_backup
-ln -s ~/git/dotfiles/zshrc ~/.zshrc
-
-mv ~/.vimrc ~/.dotfiles_backup
-ln -s ~/git/dotfiles/vimrc ~/.vimrc
-
-mv ~/.gitconfig ~/.dotfiles_backup
-ln -s ~/git/dotfiles/gitconfig ~/.gitconfig
-
-mv ~/.tmux.conf ~/.dotfiles_backup
-ln -s ~/git/dotfiles/tmux.conf ~/.tmux.conf
-
-mv ~/.pathrc ~/.dotfiles_backup
-# Since paths are site-specific, we just copy the
-# default one instead of symlinking it:
-cp ~/git/dotfiles/pathrc ~/.pathrc
+for f in $files; do
+	if [[ $f =~ ".*" ]]; then
+		src=${f/.//}
+	else
+		src=$f
+	fi;
+	if [[ ! -h ~/$f ]]; then
+		if [[ -e ~/$f && -e ${base}/${src} ]]; then
+			echo "Backing up $f"
+			mv ~/$f ${backup_dir}/$f
+		fi
+		if [[ -e ${base}/${src} ]]; then
+			#echo "Installing $f"
+			ln -s ${base}/${src} $f
+		fi
+	else
+		echo "Skipping synlink $f"
+	fi
+done;
 
 cd && source .zshrc
