9fcad4fc5fff0030fe0c4497ec4878b7b907e0be
John M. Daly <jmdaly@gmail.com>
2016-07-03 14:32:24 -0400

    Switch to GNU stow for manging configs

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -5,38 +5,13 @@
 
 declare base=${HOME}/dotfiles
 
-# First ensure that the submodules in this repo
-# are available and up to date:
 cd ${base}
-git submodule init
-git submodule update
-cd ~
 
-files=(.zshrc .vimrc .tmux.conf .gitconfig .pathrc .ctags .Xresources)
+# Set up all of the configs:
+apps=(git gnupg i3 i3blocks neovim rofi-pass tmux vim xorg zsh)
 
-declare backup_dir=~/.dotfiles_backup
-
-# Create a backup directory:
-mkdir -p ~/.dotfiles_backup
-
-for f in $files; do
-	if [[ $f =~ ".*" ]]; then
-		src=${f/.//}
-	else
-		src=$f
-	fi;
-	if [[ ! -h ~/$f ]]; then
-		if [[ -e ~/$f && -e ${base}/${src} ]]; then
-			echo "Backing up $f"
-			mv ~/$f ${backup_dir}/$f
-		fi
-		if [[ -e ${base}/${src} ]]; then
-			echo "Installing $f"
-			ln -s ${base}${src} $f
-		fi
-	else
-		echo "Skipping synlink $f"
-	fi
+for app in $apps; do
+	stow $app
 done;
 
 # Set up tmux plugin manager:
@@ -59,37 +34,6 @@ fi
 
 if [ ! -f ~/.config/nvim/autoload/plug.vim ]; then
 	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-# Set up symlinks for files that don't go
-# in the home directory:
-mkdir -p ~/.config/i3
-if [ ! -f ~/.config/i3/config ]; then
-	ln -s ~/dotfiles/i3 ~/.config/i3/config
-fi
-
-mkdir -p ~/.config/i3blocks
-if [ ! -f ~/.config/i3blocks/config ]; then
-	ln -s ~/dotfiles/i3blocks.conf ~/.config/i3blocks/config
-fi
-
-mkdir -p ~/.config/nvim
-if [ ! -f ~/.config/nvim/init.vim ]; then
-	ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
-fi
-
-mkdir -p ~/.config/rofi-pass
-if [ ! -f ~/.config/rofi-pass/config ]; then
-	ln -s ~/dotfiles/rofi-pass.config ~/.config/rofi-pass/config
-fi
-
-mkdir -p ~/.gnupg
-if [ ! -f ~/.gnupg/gpg.conf ]; then
-	ln -s ~/dotfiles/gpg.conf ~/.gnupg/gpg.conf
-fi
-
-if [ ! -f ~/.gnupg/gpg-agent.conf ]; then
-	ln -s ~/dotfiles/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 fi
 
 # Get the Base16 colour schemes
