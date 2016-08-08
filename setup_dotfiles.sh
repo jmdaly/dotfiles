d9e3216525e31fff56714195f2f5416fe4d01c04
John M. Daly <jmdaly@gmail.com>
2016-08-08 12:00:14 -0400

    Remove step to update base16 each time

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -41,8 +41,6 @@ mkdir -p ~/.config
 if [ ! -d ~/.config/base16-shell ]; then
 	git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 fi
-cd ~/.config/base16-shell
-git pull
 
 # Set up fonts
 
