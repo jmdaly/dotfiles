cd00bf25624f11748ad2338bfead39876c2c6534
Matthew Russell <mrussell@neptec.com>
2018-08-10 10:15:44 -0400

    fixup! Upgrading to 0.8.1

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Meslo LG S for Powerline:style=RegularForPowerline:size=10:antialias=true:autohint=false";
+static char *font = "Meslo LG S for Powerline:style=RegularForPowerline:size=10:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
