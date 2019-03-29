6400a07baf01ba4a7187e9dcc53f1526a9282497
Matthew Russell <mrussell@solacom.com>
2019-03-29 12:49:34 -0400

    Got powershell up with colourschemes, and oh-my-posh

---
+++ profile.ps1
@@ -0,0 +1,66 @@
+# VC Vars https://stackoverflow.com/a/2124759/1861346
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools"
+cmd /c "VsDevCmd.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
+Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
+
+# https://github.com/joonro/Get-ChildItemColor
+# verify this is the console and not the ISE
+Import-Module Get-ChildItemColor
+
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColor -option AllScope
+
+Write-Host "`nLoaded custom ls" -ForegroundColor Yellow
+
+# Helper function to set location to the User Profile directory
+function cuserprofile { Set-Location ~ }
+Set-Alias ~ cuserprofile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+
+    throw "Invalid character code $Code"
+}
+
+
+# Oh-my-posh https://github.com/JanDeDobbeleer/oh-my-posh
+Import-Module posh-git
+
+# Start SshAgent if not already
+# Need this if you are using github as your remote git repository
+if (! (ps | ? { $_.Name -eq 'ssh-agent'})) {
+    Start-SshAgent
+}
+
+Import-Module oh-my-posh
+# https://github.com/JanDeDobbeleer/oh-my-posh/tree/master/Themes
+Set-Theme robbyrussell
+
+# Aliases for git
+Install-Module git-aliases -Scope CurrentUser -AllowClobber
+
+# Override
+function gst {
+	git status -uno -sb $args
+}
