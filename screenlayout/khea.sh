8ac1aba061ecf646f026568be9d5ec117245b780
Matthew Russell <matthew.g.russell@gmail.com>
2019-12-16 11:42:34 -0500

    Adding, but haven't set up with setup_files or anything yet.  This is khea only

---
+++ screenlayout/khea.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DVI-D-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DVI-D-1 --mode 2560x1080 --pos 0x160 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 5120x232 --rotate normal
