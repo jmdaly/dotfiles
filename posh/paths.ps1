0c4eb30ce6dac497cce4631ce078e7f1f3198680
Matthew Russell <matthew.g.russell@gmail.com>
2020-02-10 08:43:38 -0500

    Implementing wslpath in more places

--- posh/paths.ps1
+++ posh/paths.ps1
@@ -9,26 +9,33 @@ if (!(Get-Variable java_jdk -Scope Global -ErrorAction SilentlyContinue))
 	# $java_jdk="C:/java-1.8.0-openjdk-1.8.0.201-2.b09.redhat.windows.x86_64"
 }
 
+if ($IsWindows) { $ = ";" } else { $path_sep = ":" }
+
 $custom_paths = @(
 	("nmap.exe",       "C:/Progra~2/Nmap"),
 	("notepad++.exe",  "C:/Progra~1/Notepa~1"),
-	("mysql.exe",      "C:/Progra~1/MariaD~1.3/bin"),
+	("mysql.exe",      "C:/Progra~1/MariaD~1.4/bin"),
 	("Code.exe",       "C:/Progra~1/MIFA7F~1"),
 	("java.exe",       "$java_jdk/bin"),
 	("javax.mail.jar", "C:/jaf-1_1_1/jaf-1.1.1")
 )
 $custom_paths | ForEach-Object {
-	if ((Test-Path $_[1]) -And ((Get-Command $_[0] -ErrorAction SilentlyContinue) -eq $null)) {
-		$env:path = $_[1] + ";$env:path"
+	$p = $_[1];
+	if ($IsLinux) { $p = $(wslpath "$p") }
+	if ((Test-Path $p) -And ((Get-Command $_[0] -ErrorAction SilentlyContinue) -eq $null)) {
+		$env:PATH = "$p${path_sep}$env:PATH"
 	}
 }
 
 $custom_paths = @(
-	"$env:homepath\utils\win"
+	"$env:homepath\utils\win",
 )
 $custom_paths | ForEach-Object {
-	if (Test-Path $_) {
-		$env:path = $_ + ";$env:path"
+	$p = $_;
+	if ($IsLinux) { $p = $(wslpath "$p") }
+
+	if (Test-Path $p) {
+		$env:PATH = "$p${path_sep}$env:PATH"
 	}
 }
 
