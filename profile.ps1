b9297217d08f775e28727c7e03216fcc2810980a
Matthew Russell <matthew.g.russell@gmail.com>
2020-03-09 11:01:32 -0400

    Removed reference to Solacom

--- profile.ps1
+++ profile.ps1
@@ -14,7 +14,6 @@ $posh_dir = $(Join-Path ${HOME} dotfiles posh)
 . "$posh_dir\git-aliases.ps1"
 . "$posh_dir\paths.ps1"
 . "$posh_dir\utils.ps1"
-. "$posh_dir\solacom.ps1"
 . "$posh_dir\docker.ps1"
 
 # vim: ts=4 sw=4 sts=0 noexpandtab ff=dos ft=ps1 :
