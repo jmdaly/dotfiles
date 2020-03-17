46bdd3ed28d1e05082afa02ec8a0e94faccd19ec
Matthew Russell <matthew.g.russell@gmail.com>
2020-03-16 22:05:06 -0400

    Small updates

--- posh/install.ps1
+++ posh/install.ps1
@@ -6,3 +6,7 @@ Install-Module -Name posh-sshell
 Install-Module -Name ThreadJob
 Install-Module posh-docker -Scope AllUsers
 Install-Module git-aliases -Scope AllUsers -AllowClobber
+
+# https://awesomeopensource.com/project/mikemaccana/powershell-profile
+Install-Module Pscx -Scope CurrentUser
+Install-Module -Name Recycle -Scope CurrentUser
