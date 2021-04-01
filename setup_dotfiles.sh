2084d8caa094707667d911fec79134366e26899f
Matthew Russell <matthew.g.russell@gmail.com>
2021-04-01 13:20:34 -0400

    Tweak

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -135,7 +135,7 @@ cd "${h}"
 
 #
 # Declare the files that we always want to copy over.
-declare -a files;
+declare -a stows;
 stows+=(zsh bash vnc gdb dircolors neovim vim tmux git p10k env-modules procs)
 
 if [[ "1" != "${skip_powerline}" ]]; then
