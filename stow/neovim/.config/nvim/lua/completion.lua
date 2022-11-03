2efefeb8f465ed85f31ee70a917cb0ae784ea61b
John M Daly <jmdaly@gmail.com>
2022-11-03 14:01:24 +0000

    Update: don't use deprecated call

--- stow/neovim/.config/nvim/lua/completion.lua
+++ stow/neovim/.config/nvim/lua/completion.lua
@@ -30,13 +30,13 @@ cmp.setup({
 
 -- Setup lspconfig.
 require('lspconfig').clangd.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
 
 require('lspconfig').pylsp.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
 
 require('lspconfig').cmake.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
