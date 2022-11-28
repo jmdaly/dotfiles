96ed710de3f5bdf1a5c22e7017f0263b025c01d7
Matthew Russell <matthew.g.russell@gmail.com>
2022-11-28 07:25:14 -0500

    Trying wide display on Ford desktop

--- stow/screenlayout/.screenlayout/ugc15x24r53.sh
+++ stow/screenlayout/.screenlayout/ugc15x24r53.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output DP-0 --primary --mode 1680x1050 --pos 0x795 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 1920x1200 --pos 3600x0 --rotate left --output DP-5 --off --output DP-6 --mode 1920x1200 --pos 1680x720 --rotate normal --output DP-7 --off
+xrandr \
+  --output DP-0 --primary --mode 1680x1050 --pos 0x795 --rotate normal \
+  --output DP-1 --off \
+  --output DP-2 --off \
+  --output DP-3 --off \
+  --output DP-4 --mode 1920x1200 --pos 3600x0 --rotate left \
+  --output DP-5 --off \
+  --output DP-6 --mode 1920x1200 --pos 1680x720 --rotate normal \
+  --output DP-7 --off
