3d66d6d8e782cc0ff6d51cec87fb35974054b8b8
John M Daly <jmdaly@gmail.com>
2024-04-03 21:26:25 +0000

    feat(neovim): add CopilotChat.nvim plugin

--- stow/neovim/.config/nvim/lua/plugins.lua
+++ stow/neovim/.config/nvim/lua/plugins.lua
@@ -139,4 +139,18 @@ require("lazy").setup({
       }
   },
 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
 })
