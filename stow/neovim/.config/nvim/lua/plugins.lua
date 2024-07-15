b7076e4f9337ba6c0d6e3fc026842a7509ca3b61
John M Daly <jmdaly@gmail.com>
2024-07-15 16:47:48 +0000

    add gitlab nvim plugin

--- stow/neovim/.config/nvim/lua/plugins.lua
+++ stow/neovim/.config/nvim/lua/plugins.lua
@@ -78,6 +78,22 @@ require("lazy").setup({
     },
   },
 
+  {
+  "harrisoncramer/gitlab.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+      "nvim-tree/nvim-web-devicons" -- Recommended but not required. Icons in discussion tree.
+    },
+    enabled = true,
+    build = function () require("gitlab.server").build(true) end, -- Builds the Go binary
+    config = function()
+      require("gitlab").setup()
+    end,
+  },
+
   -- Snippets plugin
   { "hrsh7th/vim-vsnip",
     dependencies = {
