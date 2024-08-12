13e258261775c58906946ac3c8755b7fb2ba8d7d
John M Daly <jmdaly@gmail.com>
2024-08-12 19:59:07 +0000

    chore: convert setup script to bash

--- setup.sh
+++ setup.sh
@@ -1,35 +1,25 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 # This script sets up symlinks to all the dotfiles
 # in the user's home directory.
 
-declare base=${HOME}/dotfiles
+base=${HOME}/dotfiles
 
 # Check for required dependencies before continuing:
-if [[ ! -a $(which git) ]]; then
+if ! command -v git &> /dev/null; then
   echo "Error: git is not installed. Please install git first."
   exit 1
 fi
 
-if [[ ! -a $(which curl) ]]; then
-  echo "Error: curl is not installed. Please install curl first."
-  exit 1
-fi
-
-if [[ ! -a $(which stow) ]]; then
+if ! command -v stow &> /dev/null; then
   echo "Error: stow is not installed. Please install stow first."
-  exit 1
-fi
-
-if [[ ! -a $(which unzip) ]]; then
-  echo "Error: unzip is not installed. Please install unzip first."
   exit 1
 fi
 
 # Set up tmux plugin manager:
 mkdir -p ~/.tmux/plugins
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Set up all of the configs:
@@ -40,12 +30,12 @@ cd ${base}/stow
 # it easy to add configurations for new applications
 # without having to modify this script.
 for app in */; do
-	stow -t ${HOME} $app
-done;
+  stow -t ${HOME} $app
+done
 
 # If we have bat, update the theme cache in case new themes
 # have been added
-if [[ -a $(which bat) ]]; then
+if command -v bat &> /dev/null; then
   echo "Updating bat cache"
   bat cache --build
 fi
