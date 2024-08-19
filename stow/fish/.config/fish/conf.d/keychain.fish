dab4600189b8b061599906ba57a3e7d3febf6251
John M Daly <jmdaly@gmail.com>
2024-08-19 15:07:36 -0400

    fix: fish keychain config

--- stow/fish/.config/fish/conf.d/keychain.fish
+++ stow/fish/.config/fish/conf.d/keychain.fish
@@ -1,5 +1,7 @@
 # If keychain exists, use it to manage the ssh agent
 # Only do this when the login shell is fish
-if string match -q '*fish' $SHELL and type -q keychain
-    keychain --quiet --agents ssh --quick --quiet --eval | source
+if string match -q '*fish' $SHELL
+    if type -q keychain
+        keychain --quiet --agents ssh --quick --quiet --eval | source
+    end
 end
