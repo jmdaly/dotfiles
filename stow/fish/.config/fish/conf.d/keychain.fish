2c8a4f882b036c1fd8bb6e33c6702127bd624b69
John M Daly <jmdaly@gmail.com>
2024-08-19 14:38:50 -0400

    fix: fish keychain loading

--- stow/fish/.config/fish/conf.d/keychain.fish
+++ stow/fish/.config/fish/conf.d/keychain.fish
@@ -1,5 +1,5 @@
 # If keychain exists, use it to manage the ssh agent
 # Only do this when the login shell is fish
-if string match -q fish $SHELL and type -q keychain
+if string match -q '*fish' $SHELL and type -q keychain
     keychain --quiet --agents ssh --quick --quiet --eval | source
 end
