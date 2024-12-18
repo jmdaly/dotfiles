2885e18b3ed061d6f16a5912da9b677ed4af84ff
John M Daly <jmdaly@gmail.com>
2024-12-18 12:20:42 -0500

    feat: use blink.cmp for completion

--- stow/neovim/.config/nvim/lua/plugins.lua
+++ stow/neovim/.config/nvim/lua/plugins.lua
@@ -76,26 +76,49 @@ require("lazy").setup({
 
   {
     'neovim/nvim-lspconfig', -- Configurations for neovim's language client
+    dependencies = { 'saghen/blink.cmp' },
   },
+
+  -- {
+  --   "hrsh7th/nvim-cmp", -- Autocompletion plugin
+  --   -- load cmp on InsertEnter
+  --   event = "InsertEnter",
+  --   -- these dependencies will only be loaded when cmp loads
+  --   -- dependencies are always lazy-loaded unless specified otherwise
+  --   dependencies = {
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-buffer",
+  --     "hrsh7th/cmp-vsnip",
+  --   },
+  -- },
+  --
+  -- -- Snippets plugin
+  -- { "hrsh7th/vim-vsnip",
+  --   dependencies = {
+  --     "rafamadriz/friendly-snippets",
+  --   },
+  -- },
 
   {
-    "hrsh7th/nvim-cmp", -- Autocompletion plugin
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-vsnip",
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
+    -- use a release tag to download pre-built binaries
+    version = 'v0.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = { preset = 'default' },
     },
-  },
-
-  -- Snippets plugin
-  { "hrsh7th/vim-vsnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" }
   },
 
   -- Improved Rust development
