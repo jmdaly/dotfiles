4f3f4dda4c3a1dca8523eccbb089a8f738f19946
John M Daly <jmdaly@gmail.com>
2025-04-09 17:00:31 +0000

    feat(neovim): use OSC 52 for clipboard support

--- stow/neovim/.config/nvim/init.lua
+++ stow/neovim/.config/nvim/init.lua
@@ -2,6 +2,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set up the clipboard to use osc52, which allows copying to the system clipboard
+-- This is useful when using neovim inside a docker container or a remote server.
+vim.g.clipboard = 'osc52'
+
 -- Load filetype plugins. This has to come before any autocmds
 -- that do things like change the commenstring, so we keep it near
 -- the top of the file.
