7e79ea435a990eeb88c23330acd67b77e3856b09
John M Daly <jmdaly@gmail.com>
2025-04-23 15:34:00 -0400

    chore(mise): ensure completions are set up

--- stow/fish/.config/fish/conf.d/mise.fish
+++ stow/fish/.config/fish/conf.d/mise.fish
@@ -2,4 +2,9 @@ if status is-interactive
     if type -q mise
         mise activate fish | source
     end
+    # Check if the `usage` command is available, which mise
+    # uses to generate completions.
+    if type -q usage
+        mise completion fish | source
+    end
 end
