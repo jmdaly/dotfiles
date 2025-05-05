4da8df78c7f2f6804a9417f983cea92c216d56bc
John M Daly <jmdaly@gmail.com>
2025-05-05 14:20:46 -0400

    feat(nvim): add better markdown rendering

--- stow/neovim/.config/nvim/lua/plugins.lua
+++ stow/neovim/.config/nvim/lua/plugins.lua
@@ -130,6 +130,14 @@ require("lazy").setup({
     },
   },
 
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
   -- Improved Rust development
   {
     'mrcjkb/rustaceanvim',
